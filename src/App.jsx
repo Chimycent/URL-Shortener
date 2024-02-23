897f1862fb3a84276b1cda088f2f442bd9ee98bc
Your Name <innocentchime30@gmail.com>
2024-02-23 13:25:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HeroSection from './components/HeroSection'
+import Navbar from './components/NavBar'
+
+
+function App() {
+  return (
+    <div className='bg-[#0B101B] text-white h-dvh'>
+      <Navbar/>
+      <HeroSection/>
+    </div>
+  )
+}
+
+export default App
